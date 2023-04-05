9fbf75dc4068a84d112a2c29ed1c876746f90a50
Aziz Bakirov <azizbakirov9@gmail.com>
2023-04-05 23:34:04 +0500

    pc

--- src/Components/pages/about/About.jsx
+++ src/Components/pages/about/About.jsx
@@ -5,13 +5,19 @@ import "./about.scss"
 function About() {
   return (
     <div className="about">
-      <p>
-        I live in Tashkent, Uzbekistan, I am a Front-End Developer. My skill set
-        includes designing, animating and coding products with the highest
-        precision and attention to detail. I always strive to create interfaces
-        that are not only aesthetically pleasing, but also provide a great user
-        experience.
-      </p>
+      <div className="info">
+      <h1>Aziz Bakirov</h1>
+        <p>
+          I live in Tashkent, Uzbekistan, I am a Front-End Developer. My skill
+          set includes designing, animating and coding products with the highest
+          precision and attention to detail. I always strive to create
+          interfaces that are not only aesthetically pleasing, but also provide
+          a great user experience.
+        </p>
+      </div>
+      <div className="info_img">
+        <img src="" alt="man ilustrator" />
+      </div>
     </div>
   );
 }
