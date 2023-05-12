b5c98e58edc8da848e981fc8b6fc4c1e4c210c31
TodoWeb <azizbakirov9@gmail.com>
2023-05-13 00:43:19 +0500

    about page

--- src/Components/pages/about/About.jsx
+++ src/Components/pages/about/About.jsx
@@ -1,53 +1,70 @@
 import React from "react";
 import "./about.scss";
 import myImg from "./myPhotos/man.png";
+import certificate from "./sertificate/certificate.png";
 import Footer from "../home/footer/Footer";
 
 function About() {
   return (
     <div className="about">
-      <div className="about_page">
-        <div className="info">
-          <h1>Aziz Bakirov</h1>
-          <p>
+      <div className="about_pages">
+        <div className="my_info">
+          <p className="title">Aziz Bakirov</p>
+          <p className="desc">
             I live in Tashkent, Uzbekistan, I am a Front-End Developer. My skill
             set includes designing, animating and coding products with the
             highest precision and attention to detail. I always strive to create
             interfaces that are not only aesthetically pleasing, but also
             provide a great user experience.
           </p>
-          <div className="my_skills">
-            <h2>My Skill</h2>
-            <div className="skill">
-              <a target="_blank" href="https://html5.org/">
-                <i className="fa-brands fa-html5"></i>
-              </a>
-              <a target="_blank" href="https://www.css3.com/">
-                <i className="fa-brands fa-css3-alt"></i>
-              </a>
-              <a target="_blank" href="https://sass-lang.com/">
-                <i className="fa-brands fa-sass"></i>
-              </a>
-              <a target="_blank" href="https://www.javascript.com/">
-                <i className="fa-brands fa-js"></i>
-              </a>
-              <a target="_blank" href="https://react.dev/">
-                <i className="fa-brands fa-react"></i>
-              </a>
+          <div className="skils link">
+            <p>My skills</p>
+            <div className="icon_link">
+              <i className="fa-brands fa-html5"></i>
+              <i className="fa-brands fa-css3-alt"></i>
+              <i className="fa-brands fa-sass"></i>
+              <i className="fa-brands fa-js"></i>
+              <i className="fa-brands fa-react"></i>
+              <i className="fa-brands fa-github"></i>
             </div>
           </div>
-          <div className="location">
-            <div className="location_icon">
-              <i className="fa-solid fa-location-dot"></i>
+          <div className="social_media link">
+            <div className="icon_link social">
+              <a target="_blank" href="">
+                <i className="fa-brands fa-twitter"></i>
+              </a>
+              <a
+                target="_blank"
+                href="https://www.instagram.com/azizoficiall_/"
+              >
+                <i className="fa-brands fa-instagram"></i>
+              </a>
+              <a target="_blank" href="">
+                <i className="fa-brands fa-github"></i>
+              </a>
+              <a target="_blank" href="">
+                <i className="fa-brands fa-facebook"></i>
+              </a>
+              <a target="_blank" href="">
+                <i className="fa-brands fa-linkedin"></i>
+              </a>
+              <a target="_blank" href="">
+                <i class="fa-brands fa-telegram"></i>
+              </a>
+              <a target="_blank" href=""></a>
             </div>
-            <span>
-              My address is the city of Alimkent, Oqqorgon District, Tashkent
-              Region, Uzbekistan
-            </span>
           </div>
         </div>
-        <div className="info_img">
-          <img src={myImg} alt="" />
+        <div className="my_certificate">
+          <div className="certificate">
+            <img src={certificate} alt="" />
+            <p className="cer_title">PDP Academy FrontEnd</p>
+            <div className="found">
+              <img src={certificate} alt="" />
+              <img src={certificate} alt="" />
+              <img src={certificate} alt="" />
+            </div>
+          </div>
         </div>
       </div>
       <Footer />
